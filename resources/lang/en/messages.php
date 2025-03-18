078ae21c5800acbbcc7337d3bfa9ccf4377674ec
DanielDdungu <daddungu@outlook.com>
2025-03-18 19:16:46 +0100

    uploaded .yml file

---
+++ resources/lang/en/messages.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'welcome' => 'Welcome to our website!',
+    'login' => 'Login',
+    'logout' => 'Logout',
+    'register' => 'Register',
+    'password' => 'Password',
+    'email' => 'Email Address',
+    'error' => 'Something went wrong, please try again.',
+];
